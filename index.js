4ea0814f9108d7cbe1af075d701148773855df5c
Léopoldine Brion <leopoldine.brion@outlook.fr>
2022-06-23 10:19:06 +0200

    test effect apparition

---
+++ index.js
@@ -0,0 +1,21 @@
+const ratio = .5
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+  
+const handleIntersect = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        }
+    });
+}
+
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('[class*="reveal-"]').forEach(r => {
+    observer.observe(r)
+});
